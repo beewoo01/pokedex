288f86d631992091115cbb6e7310b2bde251cfd4
hyunwoo <beewoo01@naver.com>
2025-07-24 11:17:24 +0900

    Chore: change router setting

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,10 +1,7 @@
 import 'package:flutter_pokedex/presentation/type_router.dart';
 import 'package:go_router/go_router.dart';
 
-final router = GoRouter(initialLocation: SPLASH, routes: [
-  // TypedGoRoute<SplashRoute>(path: SPLASH),
-
-]);
+final router = GoRouter(initialLocation: SPLASH, routes: $appRoutes);
 
 
 
