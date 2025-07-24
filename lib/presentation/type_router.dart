ef4b410e3f3207bf10848d7576b2f302f0813295
hyunwoo <beewoo01@naver.com>
2025-07-24 11:06:39 +0900

    Chore: set router

---
+++ lib/presentation/type_router.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/presentation/screen/splash_screen.dart';
+import 'package:go_router/go_router.dart';
+part 'type_router.g.dart';
+
+@TypedGoRoute<SplashRoute>(path: '/splash')
+class SplashRoute extends GoRouteData with _$SplashRoute {
+  const SplashRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      const SplashScreen();
+}
