f8299d785f2e512af28b63dea9a8ba7f99c2fa4d
hyunwoo <beewoo01@naver.com>
2025-07-25 22:34:36 +0900

    Feature: modulelyze Network image

---
+++ lib/presentation/component/network_image.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/logger.dart';
+
+class NetworkImage extends StatelessWidget {
+  final String imageUrl;
+  final BoxFit fit;
+
+  const NetworkImage({
+    super.key,
+    required this.imageUrl,
+    this.fit = BoxFit.fitHeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: fit,
+      errorBuilder: (context, error, stackTrack) {
+        logger.e("error: $error");
+        logger.e("stackTrack: $stackTrack");
+
+        return Icon(Icons.error);
+      },
+    );
+  }
+}
