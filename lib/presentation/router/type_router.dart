d63b2171f5bc834be4cd2926d85bb3eb4805e42d
hyunwoo <beewoo01@naver.com>
2025-07-25 14:22:50 +0900

    Feature: onbaording

--- lib/presentation/router/type_router.dart
+++ lib/presentation/router/type_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/presentation/router/route_path.dart';
+import 'package:flutter_pokedex/presentation/screen/onboarding/on_boarding_root.dart';
 import 'package:flutter_pokedex/presentation/screen/splash/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,4 +13,13 @@ class SplashRoute extends GoRouteData with _$SplashRoute {
   @override
   Widget build(BuildContext context, GoRouterState state) =>
       const SplashScreen();
+}
+
+@TypedGoRoute<OnBoardingRoute>(path: RoutePath.onBoarding)
+class OnBoardingRoute extends GoRouteData with _$OnBoardingRoute {
+  const OnBoardingRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      const OnBoardingRoot();
 }
--- lib/presentation/screen/onboarding/on_boarding_root.dart
+++ lib/presentation/screen/onboarding/on_boarding_root.dart
@@ -1,31 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/presentation/theme/common_color.dart';
+import 'package:flutter_pokedex/presentation/theme/text_theme.dart';
 
 class OnBoardingRoot extends StatelessWidget {
   const OnBoardingRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      body: ColoredBox(
-        color: Colors.white,
-        child: Column(
-          children: [
-            const Spacer(),
-            Align(
-              alignment: Alignment.center,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left : 10,
-                    child: Image.network(
-                      "https://play.pokemonshowdown.com/sprites/trainers/bugcatcher-gen4dp.png",
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              /*Image.network(
+                "https://play.pokemonshowdown.com/sprites/trainers/bugcatcher-gen4dp.png",
+              ),*/
+              SizedBox(
+                width: double.infinity,
+                height: 264,
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      left: 10,
+                      bottom: 0,
+                      right: 110,
+                      child: Image.network(
+                        "https://play.pokemonshowdown.com/sprites/trainers/bugcatcher-gen4dp.png",
+                        fit: BoxFit.fitHeight,
+                      ),
                     ),
-                  ),
-                ],
+
+                    Positioned.fill(
+                      bottom: 0,
+                      right: 1,
+                      left: 102,
+                      child: Image.network(
+                        "https://play.pokemonshowdown.com/sprites/trainers/birch.png",
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 19.5),
+                  child: Text(
+                    "Todos os Pokémons em um só Lugar",
+                    style: textTheme.displayMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 19.5),
+                  child: Text(
+                    "Acesse uma vasta lista de Pokémon de todas as gerações já feitas pela Nintendo",
+                    style: textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+
+              PageIndicator()
+            ],
+          ),
         ),
       ),
     );
