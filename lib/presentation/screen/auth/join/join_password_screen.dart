482d9f0d94c3472f80b66c5ae3e6ecaaf57b6d27
hyunwoo <beewoo01@naver.com>
2025-07-27 12:40:54 +0900

    Feature: Add Join password screen

---
+++ lib/presentation/screen/auth/join/join_password_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JoinPasswordScreen extends StatelessWidget {
+  const JoinPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
